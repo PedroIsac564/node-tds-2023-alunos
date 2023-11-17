84943a99a75ba83422fdc388fad9a3cd78b102cb
Pedro <pedrosouza.senai564@gmail.com>
2023-11-17 08:23:31 -0300

    feat: metodo lista

---
+++ src/models/roupas/RoupasLista.js
@@ -0,0 +1,31 @@
+export class RoupaLista {
+    constructor() {
+        this.roupas = [];
+    }
+    getAllRoupas() {
+        return this.roupas;
+    }
+    addRoupa(roupa) {
+        this.roupas.push(roupa)
+    }
+    getRoupa(id) {
+        return this.roupas.find((roupa) => roupa === id)
+    }
+    updateRoupa(id, nome, tamanho, cor, quantidade, imagemUrl) {
+        this.roupas = this.roupas.map((roupa) => {
+            if (roupa.id === id) {
+                roupa.nome = nome;
+                roupa.tamanho = tamanho;
+                roupa.cor = cor;
+                roupa.quantidade = quantidade;
+                roupa.imagemUrl = imagemUrl;
+            }
+            return this.getRoupa(id)
+        });
+    }
+    removeRoupa(id) {
+        this.roupas = this.roupas.
+            filter(roupa => roupa.id !== id);
+    }
+
+}
